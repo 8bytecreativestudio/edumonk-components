2f5b16a9c575bd6a867f99521dfbf34318fbcc8e
Bipin Bhandari <bookbipii@gmail.com>
2018-05-01 19:57:04 +0545

    basic layout components created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import 'antd/dist/antd.css';
 import HomePage from './modules/practice/pages/HomePage';
+import ChatsPage from './modules/practice/pages/ChatsPage';
 
 export default () => (
   <Router>
-    <Route exact path="/" component={HomePage} />
+    <div>
+      <Route exact path="/" component={HomePage} />
+      <Route exact path="/chats" component={ChatsPage} />
+    </div>
   </Router>
 );
