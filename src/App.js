4d1467d7d7825be093f528aa92a26036b184b07a
kabir <kabirpokharel12@gmail.com>
2018-05-02 13:13:17 +0545

    import errors were fixed

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import 'antd/dist/antd.css';
-import HomePage from './modules/practice/pages/HomePage';
-import ChatsPage from './modules/practice/pages/ChatsPage';
+// import HomePage from './modules/practice/pages/HomePage';
+// import ChatsPage from './modules/practice/pages/ChatsPage';
+import One from './modules/practice/pages/One';
 
 export default () => (
   <Router>
     <div>
-      <Route exact path="/" component={HomePage} />
-      <Route exact path="/chats" component={ChatsPage} />
+      <Route exact path="/" component={One} />
     </div>
   </Router>
 );
